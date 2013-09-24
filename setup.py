4585a88c9904c2081fea628a8c25f0ddc8a287cc
Vladislav Lozhechnik <=>
2013-09-24 17:41:38 +0300

    modify documentation

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django_relinking',
     version='0.0.1',
-    packages=['django_relinking'],
+    packages=['django_relinking', 'south'],
     include_package_data=True,
     license='BSD License',
     description='Provide relinking features.',
